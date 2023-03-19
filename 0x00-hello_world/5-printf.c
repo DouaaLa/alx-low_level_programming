8ba5dfb5b0bd59743a24da0f4fd8a4101e7df820
DouaaLa <douaalakrati75@gmail.com>
2023-03-19 15:35:29 +0000

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,10 +5,12 @@
  * Return: Always 0 (Success)
  */
 int main(void)
+
 {       
-	char message[]="with proper grammar, but the outcome is a piece of art,";
+	char message[] = "with proper grammar, but the outcome is a piece of art,";
 
 
-	printf("%s\n",message);
+	printf("%s\n", message);
+	
 	return (0);
 }
