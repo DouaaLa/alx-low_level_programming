ddeb43ddfb27665b83b7b91ed4d7d0e7ce29f277
DouaaLa <douaalakrati75@gmail.com>
2023-03-19 15:20:55 +0000

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{   
+    char output[]="Programming is like building a multilingual puzzle";
+
+
+    puts(output);
+    
+    return (0);
+}
