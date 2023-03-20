e5a27cf9f96b8a0ee1472096fc4063c3cf8dda38
DouaaLa <douaalakrati75@gmail.com>
2023-03-20 20:15:02 +0000

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,13 +15,13 @@ int main(void)
 	{
 		for (n2 = n1 + 1; n2 <= 9; n2++)
 		{
-				putchar(48 + n1);
-			        putchar(48 + n2);
-			        if (n1 < 8)
-				{
-					putchar(',');
-					putchar(' ');
-				}
+			putchar(48 + n1);
+			putchar(48 + n2);
+			if (n1 < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
