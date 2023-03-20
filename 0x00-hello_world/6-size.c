98b5b2a4ad981b43a68e7744caeb7386f8771d19
DouaaLa <douaalakrati75@gmail.com>
2023-03-20 21:46:58 +0000

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Access 0 (Success)
+ */
+int main(void)
+{
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of a int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n",sizeof(long));
+	printf("Size of a float: %zu byte(s)\n",sizeof(float));
+	printf("Size of a long long int: %zu byte(s)",sizeof(double));
+	return (0);
+}
+
