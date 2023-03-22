4e67f48a98df7fe01ae43dd988a697d91ec662f4
DouaaLa <douaalakrati75@gmail.com>
2023-03-22 18:54:09 +0000

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_sign - dqzdqzdqzdqzd
  * @n: Description of parameter x
@@ -12,18 +13,17 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-	        return (1);
+		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
-	        return (0);
+		return (0);
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
-	        return (-1);
+		return (-1);
 	}
 	return (0);
 }
-
