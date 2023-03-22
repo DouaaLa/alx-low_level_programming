1d46f5deda21b432e0da58aca25f59c38c7f53b4
DouaaLa <douaalakrati75@gmail.com>
2023-03-22 17:50:13 +0000

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		if (c == i )
+			return 1;
+		else
+			return 0;
+	}
+}
