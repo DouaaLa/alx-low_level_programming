cce306000cef414e19b7d644c3bcb48d9d427c79
DouaaLa <douaalakrati75@gmail.com>
2023-04-01 00:03:01 +0000

    new commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,19 +8,17 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-
-	while (*s1 != '\0' &&  *s2 != '\0' && *s1 == *s2)
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
 	{
 		s1++;
 		s2++;
 	}
-	if (*s1 != *s2)
+	if (*s1 == *s2)
 	{
-		return (*s1 - *s2);
+		return (0);
 	}
 	else
 	{
-		return (0);
+	return (*s1 - *s2);
 	}
 }
