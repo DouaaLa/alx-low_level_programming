334ac946c73d9a1e368a313757d5ab8054576fc3
DouaaLa <douaalakrati75@gmail.com>
2023-04-06 06:36:15 +0000

    Print characters recursively

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/** 
+/**
  * _puts_recursion - prints a string followed by a new line
  * @s: string
  *
@@ -9,11 +9,11 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s == 0)
 	{
+		_putchar('\0');
 		return;
 	}
 	_putchar(*s);
 	_puts_recursion(s + 1);
-	_putchar('\0');
 }
