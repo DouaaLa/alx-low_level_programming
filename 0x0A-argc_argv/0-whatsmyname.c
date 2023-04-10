11d1010016a0f8b44ac92d5b8122415a72d32986
DouaaLa <douaalakrati75@gmail.com>
2023-04-10 23:48:35 +0000

    mynameis

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ * @argc: integr, number of arguments
+ * @argv: string containing the values of the array
+ * Return: On success 0
+ * On error exit.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s \n",*(argv + 0));
+	return (0);
+}
