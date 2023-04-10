52bacbb188f298a3f1dcf116d986a1620cfc79d5
DouaaLa <douaalakrati75@gmail.com>
2023-04-10 15:10:48 +0000

    static library

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,3 +1,21 @@
 #include "main.h"
 
-char *_strchr(char *s, char c);
+/**
+ * _strchr - fills memory with a constant byte.
+ * @s: first bytes of the memory
+ * @c: constant byte b
+ * Return: Null when not found
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return ('\0');
+}
