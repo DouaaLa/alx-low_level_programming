41318cde91f74d5d34ff98cd958ba0a8b29d7b93
DouaaLa <douaalakrati75@gmail.com>
2023-04-11 00:15:05 +0000

    print numer of arg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *main - print argument
+ *@argc: integer
+ *@argv: string
+ *Return: 0
+ *
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
+
