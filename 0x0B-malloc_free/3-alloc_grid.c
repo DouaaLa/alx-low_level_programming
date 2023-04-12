55e7a943d5a125cbb726cc8ff8cd48a19c93263a
DouaaLa <douaalakrati75@gmail.com>
2023-04-12 18:13:18 +0000

    new commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,17 +9,16 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int **T, i, j;
+	int i, j;
 
-	T = malloc(sizeof(int) * height);
+	int **T = (int **)malloc(sizeof(int) * height);
 	if (width <= 0 || height <= 0 || T == NULL)
 	{
 		return (NULL);
 	}
 	for (i = 0; i < height; i++)
 	{
-		T[i] = malloc(width * sizeof(int));
-
+		T[i] = (int *)malloc(sizeof(int) * width);
 		if (T[i] == NULL)
 		{
 			for (j = 0; j < i; j++)
@@ -30,6 +29,9 @@ int **alloc_grid(int width, int height)
 			return (NULL);
 		}
 	}
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			T[i][j] = 0;
 
 	return (T);
 }
