728155b0d046c9405c2737e624d256b784bdf86f
DouaaLa <douaalakrati75@gmail.com>
2023-04-12 22:25:00 +0000

    new commit

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * free_grid - frees a grid
+ * @grid: integer
+ * @height: integer
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
