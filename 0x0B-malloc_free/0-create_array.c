0b85ff3e84361acb8c3cf8716ee36e5d1879787b
DouaaLa <douaalakrati75@gmail.com>
2023-04-12 01:36:37 +0000

    sting like a bee

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * create_array - create a dynamic array
+ * @size: Integer, size of array
+ * @c: char
+ * Return: pointer to the array
+ * On error it returned NULL.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr_s;
+	int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	arr_s = (char *)malloc(size * sizeof(char));
+	if (arr_s == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		*(arr_s + i) = c;
+	}
+	return (arr_s);
+}
