a2a53af7b738900323061681eaaf34dbc3a57187
DouaaLa <douaalakrati75@gmail.com>
2023-04-15 22:36:41 +0000

    multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *main - multiplies two passed arguments
+ *@argc: integer
+ *@argv: string
+ *Return: 0
+ *
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, mul;
+
+	while (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	mul = a * b;
+	printf("%d\n", mul);
+	return (0);
+}
